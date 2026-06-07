394f89ee42d321f87a4f09024bcf2fecb668de4e
Alina Andreeva <andreeva.a.wezom@gmail.com>
2026-06-07 19:59:57 +0300

    no-cache

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,7 +4,7 @@ import ProductsList from './_components/ProductsList';
 export const dynamic = 'force-dynamic';
 
 export default async function Products() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/products`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/products`, { cache: 'no-cache' });
   const productsData: Product[] = await response.json();
 
   const cartResponse = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/users/2/cart`, { cache: 'no-cache' });
